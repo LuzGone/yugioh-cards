8bbc8f5027f9df02ab782d1909d913bd187760ba
Luiz Gonzaga <luizg.academico@gmail.com>
2023-06-03 19:59:11 -0300

    Editar e Excluir Prontos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,15 +12,18 @@ export class AppComponent {
   cards:YuCard[];
   tipos:string[]=['Aqua','Demônio','Dragão','Besta','Besta Alada', 'Guerreiro', 'Máquina','Planta','Piro','Rocha','Trovão','Zumbi','Ciberso','Mago','Dinossauro','Fada','Besta-Guerreira','Besta Divina','Inseto','Peixe','Psíquico','Réptil','Serpente Marinha','Wyrm','Deus Criador'];
   atributos:string[]=['Água','Fogo','Luz','Terra','Trevas','Vento','Divino'];
-
+  isEditing:boolean = false;
   constructor(){
     this.yuCard = new YuCard();
     this.cards = [];
   }
 
   addCard(){
-    this.cards.push(this.yuCard);
+    if(!this.isEditing){
+      this.cards.push(this.yuCard);
+    }
     this.yuCard = new YuCard();
+    this.isEditing = false;
   }
 
   listCards(){
@@ -30,6 +33,7 @@ export class AppComponent {
   editCard(nome:string){
       const index:number = this.cards.findIndex(yuCard=>yuCard.nome==nome);
       this.yuCard = this.cards[index];
+      this.isEditing = true
   }
 
   deleteCard(nome:string){
